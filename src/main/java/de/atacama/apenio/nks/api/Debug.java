6aaf909a7a5fb1471da32e49ac33aa9afae26f47
Simon Auchter <auchter@atacama.de>
2019-02-27 15:32:32 +0100

    Entfernen unnötiger Konstruktoren und beheben eines fehlers in der path Variable.

--- src/main/java/de/atacama/apenio/nks/api/Debug.java
+++ src/main/java/de/atacama/apenio/nks/api/Debug.java
@@ -40,7 +40,10 @@ public class Debug {
 		
 		//System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(response));
 		//System.out.println("Results: " + response.size());
-        NksResponse response = Nks.newConnection("http://apenioapp02:19080").prepareRequest().get().appliances().cName("IA123").list().execute();
+        //NksResponse response = Nks.newConnection("http://apenioapp02:19080").prepareRequest().get().appliances().cName("IA123").execute();
+        NksResponse response = Nks.newConnection("http://apenioapp02:19080").prepareRequest().access().element().createSimpleQuery().addConcept("IA123").done().execute();
+        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(response));
+        System.out.println("Results: " + response.size());
 	}
 
 }
